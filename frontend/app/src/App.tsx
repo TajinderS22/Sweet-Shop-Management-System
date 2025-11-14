f71f2f3f615dacd2853be91683104d58e59046fe
Tajinder Singh <singhtajinder9888@gmail.com>
2025-11-14 22:44:43 +0530

    Init sweets shop management system skeleton of frontend and backend

---
+++ frontend/app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-4xl text-green-400 bg-black'>
+        hi
+      </div>
+    </>
+  )
+}
+
+export default App
